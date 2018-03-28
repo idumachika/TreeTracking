270f42c3b6b2f3486943d8f5f114bd48b2cff529
devDonald <donaldebuga@gmail.com>
2018-03-28 13:18:06 +0100

    Donald commit

---
+++ app/src/main/java/com/example/iduma/tree_tracking/Activities/ReportDeforestation.java
@@ -0,0 +1,17 @@
+package com.example.iduma.tree_tracking.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.iduma.tree_tracking.R;
+
+public class ReportDeforestation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_deforestation);
+
+
+    }
+}
