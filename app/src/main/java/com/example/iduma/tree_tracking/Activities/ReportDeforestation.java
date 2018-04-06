6674c0047a42eb3531e2d09d908356b3a2acfa94
kingswhale <hardeyreemyarfyc@gmail.com>
2018-04-06 18:18:23 +0100

    commint to merge

--- app/src/main/java/com/example/iduma/tree_tracking/Activities/ReportDeforestation.java
+++ app/src/main/java/com/example/iduma/tree_tracking/Activities/ReportDeforestation.java
@@ -2,11 +2,14 @@ package com.example.iduma.tree_tracking.Activities;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomSheetBehavior;
 import android.support.design.widget.FloatingActionButton;
@@ -15,7 +18,10 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.example.iduma.tree_tracking.R;
@@ -41,6 +47,7 @@ public class ReportDeforestation extends FragmentActivity implements OnMapReadyC
     static final int REQUEST_LOCATION = 1;
     LocationManager locationManager;
     Geocoder geocoder;
+    ImageView sel_image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,8 +73,60 @@ public class ReportDeforestation extends FragmentActivity implements OnMapReadyC
         });
 
         locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        getLocation();
+
+        sel_image = (ImageView)findViewById(R.id.sel_image);
+        Button btnPicture = (Button)findViewById(R.id.btnPicture);
+        btnPicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,0);
+            }
+        });
+
     }
 
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+        sel_image.setImageBitmap(bitmap);
+    }
+
+    void getLocation() {
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+
+        } else {
+            Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+            if (location != null){
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+
+                ((EditText)findViewById(R.id.incident_latitude)).setText("Lat: " + latitude);
+                ((EditText)findViewById(R.id.incident_longitude)).setText("Long: " + longitude);
+            } else {
+                ((EditText)findViewById(R.id.incident_latitude)).setText("No location found.");
+                ((EditText)findViewById(R.id.incident_longitude)).setText("No location found.");
+            }
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,@NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case REQUEST_LOCATION:
+                getLocation();
+                break;
+        }
+    }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
