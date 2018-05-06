918055bc054cb41934eb7549e3b120cdcc88fd1d
devDonald <donaldebuga@gmail.com>
2018-05-06 21:19:53 +0100

    Afforestation statistics fixed

--- app/src/main/java/com/example/iduma/tree_tracking/Activities/Statistics.java
+++ app/src/main/java/com/example/iduma/tree_tracking/Activities/Statistics.java
@@ -2,10 +2,12 @@ package com.example.iduma.tree_tracking.Activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.iduma.tree_tracking.Model.PlantingModel;
 import com.example.iduma.tree_tracking.R;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -18,6 +20,8 @@ public class Statistics extends AppCompatActivity {
     private Button btnRetrieve;
     private TextView tvAfforest;
     private DatabaseReference treeRef;
+    private String nooftree;
+    private int treeno;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +36,14 @@ public class Statistics extends AppCompatActivity {
         treeRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
+                treeno=0;
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    PlantingModel model = ds.getValue(PlantingModel.class);
+                    nooftree =model.getNoOfTrees();
+                    treeno=treeno+Integer.parseInt(nooftree);
+                    Log.d("TAG",""+treeno);
 
+                }
             }
 
             @Override
@@ -45,7 +56,7 @@ public class Statistics extends AppCompatActivity {
         btnRetrieve.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                tvAfforest.setText(""+treeno);
             }
         });
 
